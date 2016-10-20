c1fefe0ab908d11027bebdd9c79c0d872e102940
Noski <hante_hunter@163.com>
2016-10-20 20:04:26 +0800

    add some responsive codes

--- public/javascripts/book.js
+++ public/javascripts/book.js
@@ -25,15 +25,16 @@ function renderBook (bookJSON) {
   $.each(bookJSON.books, function (index, book) {
     var $section = $('<section></section>'),
       $header = $('<header></header>'),
-      $title = $('<a class="bookTitle" href="' + book.alt + '">' + book.title + '</a>'),
+      $title = $('<a class="bookTitle" href="' + book.alt + '" target="_blank">' + book.title + '</a>'),
       $author = $('<span class="bookAuthor">作者: ' + (book.author[0] || '无') + '</span>');
       $image = $('<img src="' + book.image + '" />'),
       $p = $('<p>' + book.summary + '</p>'),
-      $tags = '',
-      $addBtn = $('<button type="button" class="addBtn">收藏本书</button>');
+      $tags = $('<div class="bookTags"></div>'),
+      $doubanBtn = $('<button type="button" class="bookBtn doubanBtn">前往豆瓣</button>'),
+      $addBtn = $('<button type="button" class="bookBtn addBtn">收藏本书</button>');
 
     $.each(book.tags, function (index, tag) {
-      $tags += '<span class="book_tag">' + tag.name + '</span>';
+      $tags.append('<span class="bookTag">' + tag.name + '</span>');
     });
 
     $addBtn.click(function () {
@@ -53,8 +54,8 @@ function renderBook (bookJSON) {
       });
     });
 
-    $header.append($title, $author, $image);
-    $section.append($header, $addBtn, $tags, $p);
+    $header.append($title, $author, $image, $addBtn);
+    $section.append($header, $tags, $p);
 
     $('#books').append($section);
 
