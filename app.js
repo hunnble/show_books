e52a52a89f823e7c593a26202285be643529f244
Noski <hante_hunter@163.com>
2016-11-03 17:05:54 +0800

    fix log of bugs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-// var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -18,8 +18,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,6 +35,7 @@ app.use(session({
 }));
 app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/images/comic_book.ico'));
 
 app.use('/', routes);
 app.use('/users', users);
---
+++ public/javascripts/error.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  var center = document.querySelector('.center');
+  var bgImg = document.querySelector('.bgImg');
+
+  center.style.padding = 0;
+  center.style.width = '100%';
+  bgImg.style.backgroundColor = '#fff';
+};
